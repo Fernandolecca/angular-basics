541d170fa40ed958e52f1d82bab50759d6a92428
Fernando lecca <fleccagu@everis.com>
2021-04-01 22:06:46 -0500

    creando modulos de contador y heroes

---
+++ src/app/contador/contador/counter.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "app-counter",
+    template: `
+        <h1>{{ title }}</h1>
+        <h3>
+        La base es: 
+        <strong>{{ base }}</strong>
+        </h3>
+        
+        <button (click)="acumulate(base)">+ {{ base }}</button>
+        <span>{{ counter }}</span>
+        
+        <button (click)="acumulate(-base)">- {{ base }}</button>
+    `
+})
+export class CounterComponent {
+  title: string = 'Contador App';
+  counter: number = 0;
+  base: number = 5;
+
+  acumulate(value: number): void {
+    this.counter += value;
+  }
+}
